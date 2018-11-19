e97a7c3fc582fbdd300d93cc0aa5ae0f9606a048
faizaan09 <faizaancharania@gmail.com>
2018-11-18 20:11:21 -0500

    about to put things together

--- Config.py
+++ Config.py
@@ -3,11 +3,11 @@ ROOT = "ROOT"
 NULL = "NULL"
 NONEXIST = -1
 
-max_iter = 5501
+max_iter = 1001
 batch_size = 10000
 hidden_size = 100
 embedding_size = 50
-learning_rate = 0.003
+learning_rate = 0.1
 display_step = 100
 validation_step = 500
 n_Tokens = 48
